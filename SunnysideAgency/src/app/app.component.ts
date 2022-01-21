d4974ce4fe5cb8f44f136c867d1be962ad37470c
Sergio Fernández <sergio.fernndez@bootcamp-upgrade.com>
2022-01-21 09:37:16 +0100

    #HeaderComponent: add good layout to order elements

--- SunnysideAgency/src/app/app.component.ts
+++ SunnysideAgency/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Sunnyside Agency';
+  public title: string = 'Sunnyside Agency';
 }
--- SunnysideAgency/src/app/header/header.component.ts
+++ SunnysideAgency/src/app/header/header.component.ts
@@ -13,3 +13,4 @@ export class HEADERComponent implements OnInit {
   }
 
 }
+
