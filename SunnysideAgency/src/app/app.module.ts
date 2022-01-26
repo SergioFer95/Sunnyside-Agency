7770c05861fee44b2b07ccc5037b002bdeb28219
Sergio Fernández <sergio.fernndez@bootcamp-upgrade.com>
2022-01-26 19:25:30 +0100

    Add normalize to INDEX.html

--- SunnysideAgency/src/app/app.module.ts
+++ SunnysideAgency/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,7 +13,8 @@ import { HEADERComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
