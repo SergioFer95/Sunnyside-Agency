4d915b47402557bb71da360ffc42aac7ffa143fe
Sergio Fernández <sergio.fernndez@bootcamp-upgrade.com>
2022-02-02 08:54:40 +0100

    Create the route from Contact btn to formComponent

--- SunnysideAgency/src/app/app-routing.module.ts
+++ SunnysideAgency/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { ContactFormComponent } from './header/contact-form/contact-form.component';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {path: '', component: AppComponent},
+  {path: 'Home', component: AppComponent},
   {path: 'Contact', component: ContactFormComponent},
   {path: '**', component:AppComponent}
 ];
